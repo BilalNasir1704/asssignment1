172be367c395b9d62bb4e7810c36e97dd4bd8715
BilalNasir1704 <110089858+BilalNasir1704@users.noreply.github.com>
2023-07-20 12:43:54 +0300

    assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val born = 2000
+    val now = 2023
+    val age = (now-born)
+    print("The age is $age")
+
+
+
+}
---
+++ src/main/kotlin/hypotenuse.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val base = 4 //base
+    val base2 = base*base //A squared
+    val height = 3 //height
+    val height2 = height*height
+    val c_2 = (base*base + height*height)
+    val c = (c_2/(c))
+    print("a2 is $b_2")
+
+    fun sqrt(int base, int height)
+
+}
+
